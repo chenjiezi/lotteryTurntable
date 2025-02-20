1d6646792b047463259b2bc1db984b716721f8e9
chenjiezi <892774940@qq.com>
2025-02-20 18:22:30 +0800

    update

---
+++ README.md
@@ -0,0 +1,12 @@
+- NOTE
+  - canvas
+    - ctx.save()和ctx.restore()，在绘制图形过程中，平移、旋转、绘制文本，会影响之前画布的内容；所以在平移、旋转、绘制文本之前save，在平移、旋转、绘制文本之后restore。保证画布之前的内容不受到影响。+
+    - ctx.arc(centerX,  centerY, radius, startAngle, endAngle);绘制扇形
+    - canvas没有提供事件机制，那么我得怎么实现在画布上的点击效果
+      - 方案一：用一个div定位到画布上指定位置。由div标签提供事件机制
+      - 方案二：在canvas标签上监听事件，在画布上某个位置点击，获取坐标，碰撞检测，触发响应操作。
+  - 数学
+    - 2Π表示完成的圆的弧度
+    - 求扇形的几何中心（质心公式）
+        - ![质心公式](./image.png)
+        - centroidFator(质心因子)、centroidAngle（质心角度）
---
+++ app.js
@@ -0,0 +1,76 @@
+const { log } = require("console");
+const express = require("express");
+const querystring = require("querystring");
+const WebSocket = require("ws");
+const app = express();
+const port = 3000;
+
+// 提供静态文件
+app.use(express.static("public"));
+
+// 处理用户页面路由
+app.get("/main/:username", (req, res) => {
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+// 启动HTTP服务器
+const server = app.listen(port, () => {
+  console.log(`Server  running on http://localhost:${port}`);
+});
+
+// 创建WebSocket服务器
+const wss = new WebSocket.Server({ server });
+const users = new Map(); // 存储在线用户
+
+wss.on("connection", (socket, req) => {
+  // 从查询参数获取用户名
+  const params = querystring.parse(req.url.split("?")[1]);
+  const username = decodeURIComponent(params.username);
+
+  if (!username) {
+    socket.close();
+    return;
+  }
+
+  // 添加用户到列表
+  users.set(socket, username);
+  updateAllUsers();
+
+  // 接受客户端消息
+  socket.on("message", (message) => {
+    const data = JSON.parse(message);
+    const { type, content } = data;
+    if (type === "ready") {
+      users.forEach((username, client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(
+            JSON.stringify({
+              type: "message",
+              data: "ready",
+            })
+          );
+        }
+      });
+    }
+  });
+  // 处理连接关闭
+  socket.on("close", () => {
+    users.delete(socket);
+    updateAllUsers();
+  });
+});
+
+// 广播更新用户列表
+function updateAllUsers() {
+  const userList = Array.from(users.values());
+  users.forEach((username, client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(
+        JSON.stringify({
+          type: "userlist",
+          users: userList,
+        })
+      );
+    }
+  });
+}
